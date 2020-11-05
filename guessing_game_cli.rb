07179542ee3394df283d42ce9acd7942fb42103d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-05 19:43:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,11 +12,11 @@ user_input = gets.chomp
 
 # compare that input to the random number that has been generated
 if user_input == number
-  return You guessed the correct number!
+  return "You guessed the correct number!"
 elsif user_input != number
-  return Sorry! The computer guessed #{number}."
-elsif user_input == exit
-  return Goodbye!
+  return "Sorry! The computer guessed #{number}."
+elsif user_input == "exit"
+  return "Goodbye!"
 
   # Print out one of three statements
 
