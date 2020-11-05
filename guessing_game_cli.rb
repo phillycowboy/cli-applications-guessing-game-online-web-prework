8e8dc0e428d99112c8177d51eada709d86add5e5
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-05 19:29:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ number = rand(1...6)
 # puts "Guess a number between 1 and 6"
 
 # Capture user input from the command line
-user_input = gets.chomp
+user_input = gets.chomp.to_s
 
 # compare that input to the random number that has been generated
 if user_input == number
