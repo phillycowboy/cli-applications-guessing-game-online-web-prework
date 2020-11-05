0bb7060c42d634c025cdf6c8ad718b3cb504a4c5
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-05 20:23:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -15,7 +15,7 @@ if user_input == "exit"
   puts "Goodbye!"
 elsif user_input != number
   puts "Sorry! The computer guessed #{number}."
-elsif user_input.to_i == number
+elsif user_input == number
   puts "You guessed the correct number!"
 
   # Print out one of three statements
